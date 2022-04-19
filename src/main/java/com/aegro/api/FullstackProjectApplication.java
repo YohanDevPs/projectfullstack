45e2db6c1f8004d586bc4c803d56c7079c57ffe2
yohan <yohan.silva@aegro.com.br>
2022-04-19 11:39:22 -0300

    build: Create farm entitie

--- src/main/java/com/aegro/api/FullstackProjectApplication.java
+++ src/main/java/com/aegro/api/FullstackProjectApplication.java
@@ -1,10 +1,20 @@
 package com.aegro.api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class FullstackProjectApplication {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setSkipNullEnabled(true);
+		return modelMapper;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackProjectApplication.class, args);
---
+++ src/main/java/com/aegro/api/repository/FarmRepository.java
@@ -0,0 +1,11 @@
+package com.aegro.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.aegro.api.entities.Farm;
+
+@Repository
+public interface FarmRepository extends JpaRepository<Farm, Long>{
+		
+}
